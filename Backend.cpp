08ec32a889c49dc5aaf4f17f0c94c6bbb9000620
k4hfie <muhammadkahfiealbintuny2006@mail.ugm.ac.id>
2025-11-27 10:40:53 +0700

    Update Backend.cpp
    made things easier to read, tidy up some stuff, added a print vector function

--- Backend.cpp
+++ Backend.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 #include <bits//stdc++.h>
 
 using namespace std;
@@ -7,20 +7,29 @@ using namespace std;
 //Vector Class
 class VectorType
 {
-    public:
+    private:
     //Parameters
-        int x_magnitude;
-        int y_magnitude;
-        int z_magnitude;
+        int x_mag;
+        int y_mag;
+        int z_mag;
     
-    //Contructor, Standard Vector
-    VectorType();
-    VectorType(int x_magnitude, int y_magnitude, int z_magnitude)
-    {
-        this->x_magnitude = x_magnitude;
-        this->y_magnitude = y_magnitude;
-        this->z_magnitude = z_magnitude;    
-    }
+    public:
+    //Default Contructor
+    VectorType():
+        x_mag(0), y_mag(0), z_mag(0){}
+
+    //Parameterized Constructor
+    VectorType(int x, int y, int z): 
+        x_mag(x), y_mag(y), z_mag(z){}
+
+    //Show Vector Function
+     void printVector()
+     {
+        cout << "Vector = [" 
+             << x_mag << ", "
+             << y_mag << ", "
+             << z_mag << "]";
+     }
 };
 
 //DEBUGGING AND TESTING
@@ -28,11 +37,6 @@ int main()
 {
     int x,y,z;
     cin >> x >> y >> z;
-    VectorType vector1(x, y, z);
-    
-    cout << vector1.x_magnitude << "\n";
-    cout << vector1.y_magnitude << "\n";
-    cout << vector1.z_magnitude << "\n";
-
-    
+    VectorType v1(x, y, z);
+    v1.printVector();
 }
